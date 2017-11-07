9866d144e919472ebfca2836868a6c47b440d4a2
Cristiano Gomes <crgomes@microsoft.com>
2017-11-07 14:23:51 -0200

    novo commit

--- DevOpsDays/DevOpsDays/App.xaml.cs
+++ DevOpsDays/DevOpsDays/App.xaml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 using Xamarin.Forms;
 
@@ -12,8 +15,10 @@ namespace DevOpsDays
         public App()
         {
             InitializeComponent();
-
             MainPage = new DevOpsDays.MainPage();
+            MobileCenter.Start("android=bea4db64-a728-421a-92af-caa6d2e2db87;" + "uwp=bea4db64-a728-421a-92af-caa6d2e2db87;" +
+                   "ios=bea4db64-a728-421a-92af-caa6d2e2db87",
+                   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnStart()
